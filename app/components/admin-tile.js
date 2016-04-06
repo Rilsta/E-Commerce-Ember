6efd45ced5d6a0b2e3b7f5f50f77ca91e4e4d6a6
riley <rstarnes9@gmail.com>
2016-04-06 11:47:30 -0700

    add delete product

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(product) {
+      if (confirm('Delete this Product?')) {
+        this.sendAction('destroyProduct', product)
+      }
+    },
+  }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    destroyProduct(product) {
+      product.destroyRecord();
+      this.transitionTo('admin');
+    },
+  }
 });
