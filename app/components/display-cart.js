6e1f1ceff9627b7a052f17a5d959814e8ea5ee31
riley <rstarnes9@gmail.com>
2016-04-07 16:00:44 -0700

    add total cost of cart items to display cart

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  fullCost: Ember.computed('shoppingCart.items', function() {
+    var items = this.get('shoppingCart').items;
+    var total = 0;
+    items.forEach(function(item){
+      total += parseInt(item.get('cost'));
+    });
+    return total;
+  }),
 });
